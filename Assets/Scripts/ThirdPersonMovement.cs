7ff8623582fbff388157b8ad240e23c2f59637b8
Andrew Eveld <eveldaj@hendrix.edu>
2021-03-23 01:23:47 -0500

    Added character model and animations

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class ThirdPersonMovement : MonoBehaviour
 {
     // https://www.youtube.com/watch?v=4HpC--2iowE&t=543s
+    // https://www.youtube.com/watch?v=vdOFUFMiPDU
 
     public CharacterController controller;
     public float speed = 6f;
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
+    public Animator animator;
+    public LayerMask groundLayer;
+    public float jumpForce = 7f;
+    public float gravity = 20f;
 
+
+    private void Start()
+    {
+    }
     // Update is called once per frame
     void Update()
     {
@@ -19,12 +28,34 @@ public class ThirdPersonMovement : MonoBehaviour
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
         if (direction.magnitude >= 0.1f)
         {
+            animator.SetBool("isRunning", true);
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-
-            controller.Move(direction * speed * Time.deltaTime);
+            
+            //controller.Move(direction * speed * Time.deltaTime);
+            
+        } else
+        {
+            animator.SetBool("isRunning", false);
         }
+        if (!IsGrounded())
+        {
+            direction.y -= gravity * Time.deltaTime;
+
+        }
+        //else if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    direction.y = jumpForce;
+        //}
+        controller.Move(direction * speed * Time.deltaTime);
+
+
+    }
+
+    private bool IsGrounded()
+    {
+        return controller.isGrounded;
     }
 }
