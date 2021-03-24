0b35c228216f6e332560edd2c7772bbdf80ab3d2
Andrew Eveld <eveldaj@hendrix.edu>
2021-03-23 20:29:46 -0500

    Fixed problem on test world

--- Assets/Scripts/CharacterAnimation.cs
+++ Assets/Scripts/CharacterAnimation.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CharacterAnimation : MonoBehaviour
 {
+    //https://www.youtube.com/watch?v=HVCsg_62xYw
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
